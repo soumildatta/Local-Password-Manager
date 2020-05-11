1e5149be7d61bbd88f3dd7960c6df0aa764529c1
Soumil Datta <soumildatta@gmail.com>
2020-05-11 00:46:31 -0500

    Add methods for hashing and verifying password

---
+++ hashing.py
@@ -0,0 +1,22 @@
+import os
+import hashlib
+import binascii
+
+# https://www.vitoshacademy.com/hashing-passwords-in-python/
+
+def hashPassword(password):
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+
+    return (salt + pwdhash).decode('ascii')
+ 
+def verifyPassword(stored_password, provided_password):
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+
+    pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'), salt.encode('ascii'), 100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    
+    return pwdhash == stored_password
